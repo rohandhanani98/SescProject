bcbe26f0e819ad082b2f57dc0a6dea60744ea69c
rohandhanani98 <dhananirohandhanani@gmail.com>
2024-04-24 10:09:08 +0100

    Final editing done for the project.

---
+++ src/main/java/Student_Portal/com/project/service/Student_Rohan_Service.java
@@ -0,0 +1,37 @@
+package Student_Portal.com.project.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import Student_Portal.com.project.classes.Student;
+import Student_Portal.com.project.repository.Student_Repository;
+
+@Component
+public class Student_Rohan_Service {
+	private Student_Repository  studentRepository;
+
+	public Student_Rohan_Service(Student_Repository studentRepository) {
+		super();
+		this.studentRepository = studentRepository;
+	}
+
+	public List<Student> findAllStudent() {
+		List<Student> students=studentRepository.findAll();
+		return students;
+	}
+
+	public Student findStudentById(String studentId) {
+		Student student=studentRepository.findByStudentId(studentId);
+		return student;
+	}
+
+	public Student addStudent(Student student) {
+		Student savedstudent=studentRepository.save(student);
+		return savedstudent;
+	}
+	
+	
+	
+	
+}
